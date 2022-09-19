a3c26081fcbb939adb788ad33f6785dbf972caaf
Robert Grzyb <robertgrzyb96@gmail.com>
2022-09-19 21:26:29 +0200

    create currency converter

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let heightElement = document.querySelector ("js-height")
+let form = document.querySelector(".js-form");
+let current = document.querySelector(".js-current");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let currentElement = current.value;
+  let currency = currencyElement.value;
+
+  let eur = 4.67;
+  let usd = 4.43;
+  let gbp = 5.46;
+
+  switch (currency) {
+    case "eur":
+      result = currentElement / eur;
+      break;
+    case "usd":
+      result = currentElement / usd;
+      break;
+    case "gbp":
+      result = currentElement / gbp;
+      break;
+  }
+  resultElement.innerText = `${result.toFixed(2)} ${currency}`
+});
